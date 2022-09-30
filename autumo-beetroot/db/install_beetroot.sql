6b70a603f6e9414a359e1b719cd1214d1fedab02
Michael Gasche <autumo.switzerland@gmail.com>
2022-09-30 03:05:41 +0200

    More generic initial api key

--- autumo-beetroot/db/install_beetroot.sql
+++ autumo-beetroot/db/install_beetroot.sql
@@ -104,7 +104,7 @@ INSERT INTO properties (id, name, value) values
 INSERT INTO properties (id, name, value) values
 (3,'mail.mailer', 'beetroot.web-mailer@autumo.ch');
 INSERT INTO properties (id, name, value) values
-(4,'web.json.api.key', 'xob1xob1xob1xob1xob1xob1xob1xob1');
+(4,'web.json.api.key', 'abcedfabcedfabcedfabcedfabcedfab');
 
 
 
